face4b1f2d7686b587f6b8d5cc2cf7a838006559
rahamath45 <rahmathsulaiman45@gmail.com>
2025-08-10 01:10:51 +0530

    completed the style

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,59 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [allData,setAllData] =useState([])
+
+
+ const API = "https://6894a313be3700414e13f330.mockapi.io/services";
+ useEffect(()=>{
+      fetch("https://6894a313be3700414e13f330.mockapi.io/services",{method:"GET",})
+       .then((response)=>response.json())
+       .then((data)=> setAllData(data))
+       .catch((err)=> console.log(err));
+ },[]);
   return (
     <>
-       <Search/>
          <div className="servicepage">
-           <Servicecard/>
+          <Search/>
+          <div className='bottom'>
+          {allData.map((details,idx)=>(
+              <Servicecard serviceDetails={details} key={idx}/>
+          ))}
+          </div>
         </div>
     </>
   )
 }
 
-export default App
+ export default App;
+
 function Search(){
     return(
         <div className='top'>
-        <div className='servicesearch'>
         <input type="text" placeholder='service search'/>
-        </div>
-        <button>Search</button>
+        <button className='btn'>Search</button>
     </div>
     )
 }
 
- function Servicecard(){
+ function Servicecard({serviceDetails}){  
     return(
         <div className="servicecard">
-           <div></div>
-           <div></div>
+           <div className='head'>
+            <div className='topic'>Service</div>
+             <div className='loc'>Location : {serviceDetails.location} </div>
+           </div>
+           <div className='body'>
+                 <div className='content'>
+                     <div className='locs'>Vechicle Type : {serviceDetails.vechicle_type}</div>
+                      <div className='loc'>Date of service :{new Date().toLocaleString().slice(0, 9)}</div>
+                      <div className='loc'>Date of delivery: {new Date().toLocaleString().slice(0, 9)} </div>
+                      <div className='locs'>Vechicle name: {serviceDetails.vechicle_name }</div>
+                 </div>
+                 <div className='amount'> Service amount : {serviceDetails.service_amount}</div>
+           </div>
         </div>
     )
  }
+
